cb13b23b1bb41263369613177728ac810864e2f0
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-01 14:30:59 -0400

    updated partial code in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,7 +29,7 @@ module.exports = {
     },
 
     deleteUser(req,res){
-        User.findOneAndRemove({_id: req.params.userId})
+        User.findOneAndDelete({_id: req.params.userId})
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: 'No user with that ID'})
