a5feb59dda72bf90c2d6650f9dc141538944883c
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-04 05:19:30 -0400

    completed thoughtRoutes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -10,9 +10,9 @@ const {
     deleteReaction,
 } = require('../../controllers/thoughtController')
 
-router.route('/').get(getAllThoughts).post(createThought)
-router.route('/:thoughtId').get(getSingleThought).delete(deleteThought)
-
-
+router.route('/').get(getAllThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).post(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(createReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
